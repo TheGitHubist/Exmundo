2d3bc74a5df55a2958a4d9e69c063f324fcca83b
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 13:45:11 +0100

    test connect

--- game/client.py
+++ game/client.py
@@ -5,20 +5,21 @@ port = 3945
 host = "10.5.1.44"
 pygame.init()
 
-async def inputs(writer):
-        #msgin = await aioconsole.ainput()
-        writer.write(host.encode())
+class client:
+    async def run(self):
+        self.reader, self.writer = await asyncio.open_connection(host, port)
+        if(await asyncio.gather(inputs(writer),receive(reader)) == 1):
+            exit(1)
+
+    async def inputs(writer, message):
+        writer.write(massage.encode())
         await writer.drain()
 
-async def receive(reader):
-    while True:
-        data = await reader.read(1024)
-        print(data.decode())
-
-async def main():
-    reader, writer = await asyncio.open_connection(host, port)
-    if(await asyncio.gather(inputs(writer),receive(reader)) == 1):
-        exit(1)
+    async def receive(reader):
+        while True:
+            data = await reader.read(1024)
+            print(data.decode())
 
 if __name__ == "__main__":
-    asyncio.run(main())
+    c = client()
+    asyncio.run(c.run)
