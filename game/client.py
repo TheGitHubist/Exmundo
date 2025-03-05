d6837ff949f9a795c5f34ce182f0ab7a526027b1
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 15:06:25 +0100

    test connect

--- game/client.py
+++ game/client.py
@@ -1,4 +1,5 @@
 import asyncio
+import time
 import pygame
 
 pygame.init()
@@ -7,25 +8,48 @@ class client:
     def __init__(self, message):
         self.port = 3945
         self.host = "10.5.1.44"
-        self.message = message
-        asyncio.run(self.main())
+        self.message = ""
 
-    async def inputs(self, writer):
-        while True:
-            if self.message == "":
-                writer.write(self.message.encode())
-                self.message = ""
-                await writer.drain()
+    # async def inputs(self, writer):
+    #     while True:
+    #         writer.write(self.message.encode())
+    #         self.message = ""
+    #         await writer.drain()
 
-    async def receive(self, reader):
-        while True:
-            data = await reader.read(1024)
-            print(data.decode())
+    # async def receive(self, reader):
+    #     while True:
+    #         data = await reader.read(1024)
+    #         print(data.decode())
 
     async def main(self):
         reader, writer = await asyncio.open_connection(self.host, self.port)
-        asyncio.create_task(self.inputs(writer))
-        asyncio.create_task(self.receive(reader))
+        
+        writer.write(b"Salut")
+        await writer.drain()
+
+        message = 10
+
+        while True:
+            data = await reader.read(1024)
+
+            if not data:
+                raise Exception("socket closed")
+
+            print(f"Received: {data.decode()!r}")
+
+            if messages > 0:
+                await asyncio.sleep(1)
+                writer.write(f"{time.time()}".encode())
+                await writer.drain()
+                message -= 1
+            else:
+                writer.write(b"quit")
+                await writer.drain()
+                break
+
+    async def run(self):
+        loop = asyncio.new_event_loop()
+        loop.run_until_complete(self.main())
 
 if __name__ == "__main__":
     c = client("Salut")
