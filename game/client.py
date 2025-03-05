9c8f820fd614013ab7da90694a3ed89f76d39da1
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 14:44:51 +0100

    test connect

--- game/client.py
+++ game/client.py
@@ -1,5 +1,4 @@
 import asyncio
-import socketio
 import pygame
 
 pygame.init()
@@ -9,10 +8,12 @@ class client:
         self.port = 3945
         self.host = "10.5.1.44"
         self.message = message
+        asyncio.run(self.main())
 
     async def inputs(self, writer):
         while True:
             writer.write(self.message.encode())
+            self.message = ""
             await writer.drain()
 
     async def receive(self, reader):
@@ -22,16 +23,10 @@ class client:
 
     async def main(self):
         reader, writer = await asyncio.open_connection(self.host, self.port)
-        if(await asyncio.gather(self.inputs(writer),self.receive(reader)) == 1):
-            exit(1)
+        asyncio.create_task(self.inputs(writer))
+        asyncio.create_task(self.receive(reader))
 
-    async def run(self):
-        asyncio.run(self.main())
-
-# if __name__ == "__main__":
-#     c = client("Salut")
-#     c.message("Tu vas Bien ?")
-
-sio = socketio.SimpleClient()
-sio.connect('10.5.1.44:3945')
+if __name__ == "__main__":
+    c = client("Salut")
+    c.message("Tu vas Bien ?")
 
