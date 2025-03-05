da5b326d08e7f692f45296c40d8224051060fcf7
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 15:14:52 +0100

    test connect

--- game/client.py
+++ game/client.py
@@ -8,7 +8,7 @@ class client:
     def __init__(self, message):
         self.port = 3945
         self.host = "10.5.1.44"
-        self.message = ""
+        self.message = message
 
     # async def inputs(self, writer):
     #     while True:
