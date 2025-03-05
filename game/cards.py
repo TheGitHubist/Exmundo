97ef30dcad7b53853a4c1c91d8b85a01477c88f0
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-03-05 11:12:40 +0100

    add files

---
+++ game/cards.py
@@ -0,0 +1,98 @@
+from abc import ABC, abstractmethod
+
+class MonsterCardModel(ABC):
+    def __init__(self, name, health, damage, effects):
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.effects = effects
+
+    @abstractmethod
+    def attack(self, other):
+        pass
+
+    def gain_health(self, amount):
+        self.health += amount
+
+    def lose_health(self, amount):
+        self.health -= amount
+
+    def activate_effects(self):
+        for effect in self.effects:
+            effect.activate(self)
+
+    def move(self):
+        pass
+
+class BasicMonsterCard(MonsterCardModel):
+    def __init__(self, name, health, damage, effects, rune_possession):
+        super().__init__(name, health, damage, effects)
+        self.rune_possession = rune_possession
+
+    def attack(self, other):
+        other.lose_health(self.damage)
+
+class EliteMonsterCard(MonsterCardModel):
+    def __init__(self, name, health, damage, effects, rune_possession, rune_cost):
+        super().__init__(name, health, damage, effects)
+        self.rune_possession = rune_possession
+        self.rune_cost = rune_cost
+        
+    def attack(self, other):
+        other.lose_health(self.damage)
+
+class BossMonsterCard(MonsterCardModel):
+    def __init__(self, name, health, damage, effects, rune_possession, rune_cost):
+        super().__init__(name, health, damage, effects)
+        self.rune_possession = rune_possession
+        self.rune_cost = rune_cost
+        
+    def attack(self, other):
+        other.lose_health(self.damage)
+
+class SpellCardModel(ABC):
+    def __init__(self, name, effects):
+        self.name = name
+        self.effects = effects
+    
+    @abstractmethod
+    def activate(self):
+        pass
+
+    def activate_effects(self, target):
+        for effect in self.effects:
+            effect.activate(self)
+
+class LinkSpellCard(SpellCardModel):
+    def __init__(self, name, effects, position, pointed_zones):
+        super().__init__(name, effects)
+        self.position = position
+        self.pointed_zones = pointed_zones
+
+    def activate(self):
+        for cards in self.pointed_zones:
+            self.activate_effects(cards)
+
+class AuraSpellCard(SpellCardModel):
+    def __init__(self, name, effects, position):
+        super().__init__(name, effects)
+        self.position = position
+
+    def activate(self):
+        self.activate_effects()
+
+class FieldSpellCard(SpellCardModel):
+    def __init__(self, name, effects, field):
+        super().__init__(name, effects)
+        self.field = field
+
+    def activate(self):
+        for cards in self.field:
+            self.activate_effects(cards)
+
+class NormalSpellCard(SpellCardModel):
+    def __init__(self, name, effects):
+        super().__init__(name, effects)
+
+    def activate(self):
+        self.activate_effects()
---
+++ game/mecanics.py
@@ -0,0 +1,12 @@
+class Turn:
+    def __init__(self, player):
+        self.player = player
+    
+    def drawPhase(self):
+        pass
+
+    def battleMonsters(self):
+        pass
+
+    def endPhase(self):
+        pass
