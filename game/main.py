5397348723b878861a2a8b99b9e7ddb08edb0283
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-03-05 10:24:20 +0100

    add base files - correction

--- game/main.py
+++ game/main.py
@@ -1,30 +1,11 @@
 import pygame as pg
-import socket
 
 from pathlib import Path
-
-
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 12345))
-    server_socket.listen(5)
-    print("Server started, waiting for connections...")
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Connection from {addr} has been established!")
-        client_socket.close()
-
-def connect_to_server():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
-    print("Connected to the server!")
-    client_socket.close()
 
 def main():
     pg.init()
     screen = pg.display.set_mode((1500, 900))
     running = True
-    server_thread = None
 
 
     while running:
@@ -36,23 +17,6 @@ def main():
 
         pg.display.flip()
     pg.quit()
-
-def start_server():
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server_socket.bind(('localhost', 12345))
-    server_socket.listen(5)
-    print("Server started, waiting for connections...")
-    while True:
-        client_socket, addr = server_socket.accept()
-        print(f"Connection from {addr} has been established!")
-        client_socket.close()
-
-
-def connect_to_server():
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    client_socket.connect(('localhost', 12345))
-    print("Connected to the server!")
-    client_socket.close()
 
 
 if __name__ == "__main__":
