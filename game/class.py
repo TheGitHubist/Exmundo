17fabd2e85c922690d888f1319c73f607e2589b6
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 11:24:02 +0100

    deck get card.json

--- game/class.py
+++ game/class.py
@@ -8,9 +8,14 @@ class field:
         pass
 
 class deck:
+    card_list = []
     def __init__(self, index):
-        data = json.load(open('./game/data/Deck.json'))
-        self.card_list = data['deck'][index]
+        data_list = json.load(open('./game/data/Decks.json'))
+        card_list = data_list['deck'][index]['cards']
+        for card in card_list:
+            data_card = json.load(open('./game/data/Cards.json'))
+            self.card_list.append(data_card['cards'][card])
+
 
 dk = deck(1)
 
