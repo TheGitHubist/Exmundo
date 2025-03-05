fcbda6c9bbb32456e54a4d8ebdd8af60489ba240
yanis lamliji <yanis.lamliji@ynov.com>
2025-03-05 13:57:41 +0100

    test connect

--- game/client.py
+++ game/client.py
@@ -20,7 +20,7 @@ async def receive(reader):
 
 async def main():
     reader, writer = await asyncio.open_connection(host, port)
-    if(await asyncio.gather(inputs(writer, message),receive(reader, message)) == 1):
+    if(await asyncio.gather(inputs(writer, message),receive(reader)) == 1):
         exit(1)
 
 if __name__ == "__main__":
