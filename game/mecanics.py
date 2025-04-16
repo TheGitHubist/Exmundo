47bd86d62f5123d4331fc4c872f237c8bc64f51f
TheGitHubist <131190563+TheGitHubist@users.noreply.github.com>
2025-04-16 16:31:22 +0200

    fix player turn

--- game/mecanics.py
+++ game/mecanics.py
@@ -1,14 +1,14 @@
 class Turn:
     def __init__(self, player):
         self.player = player
-    
-    # Removed drawPhase method as it is inconsistent with PlayerDeck logic and likely unused
-    # def drawPhase(self):
-    #     if self.player.deck:
-    #         card = self.player.deck.pop(0)
-    #         self.player.hand.append(card)
-    #         return card
-    #     return None
+
+    def drawPhase(self):
+        """Draw a card for the current player."""
+        if self.player.deck:
+            card = self.player.deck.pop(0)
+            self.player.hand.append(card)
+            return card
+        return None
 
     def battleMonsters(self):
         pass
